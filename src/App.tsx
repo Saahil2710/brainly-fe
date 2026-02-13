46ff9635ca70718f5a113ae231696872be106088
sahilsher2710 <sahilsher2710@gmail.com>
2026-02-13 14:36:23 +0530

    update!

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<SignUp/>}/>
-          <Route path="/" element={<SignIn/>}/>
+          <Route path="/" element={<SignUp/>}/>
+          <Route path="/signin" element={<SignIn/>}/>
           <Route path="/dashboard" element={<Dashborad/>}/>
         </Routes>
       </BrowserRouter>
